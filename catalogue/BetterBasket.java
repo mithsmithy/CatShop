c6fcfc708e6dadaf1e1939bb39fc2df9dbc385cc
miths <miths@Beast>
2024-01-10 13:23:08 +0000

    better

--- catalogue/BetterBasket.java
+++ catalogue/BetterBasket.java
@@ -2,16 +2,24 @@ package catalogue;
 
 import java.io.Serializable;
 
-/**
- * Write a description of class BetterBasket here.
- *
- * @author  Your Name
- * @version 1.0
- */
 public class BetterBasket extends Basket implements Serializable
 {
   private static final long serialVersionUID = 1L;
 
-
-
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  @Override
+  public boolean add( Product pr )
+  {
+    return super.add( pr );     // Call add in ArrayList
+  }
 }
